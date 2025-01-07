96b811021b9d7601df2414eb7e8c5085389140e2
CortexH <henrique.silveira.soares19988@gmail.com>
2025-01-07 16:53:43 -0300

    Project begin

---
+++ src/main/java/com/SistemaPagamento/SistemaPagamentoApplication.java
@@ -0,0 +1,11 @@
+package com.SistemaPagamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaPagamentoApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaPagamentoApplication.class, args);
+	}
+}
